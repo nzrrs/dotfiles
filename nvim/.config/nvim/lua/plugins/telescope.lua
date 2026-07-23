8c3fce2c275f69b7e2c61b2cc8ec4cb035ba470c
Sn <164057221+nzrrs@users.noreply.github.com>
2026-07-23 15:38:50 +0100

    Update telescope.lua

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,53 +1,59 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 
-  config = function()
-    local telescope = require("telescope")
+	config = function()
+		local telescope = require("telescope")
 
-    telescope.setup({
-      defaults = {
-        preview = {
-          treesitter = {
-            enable = false,
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				preview = {
+					treesitter = {
+						enable = false,
+					},
+				},
+			},
+			pickers = {
+				colorscheme = {
+					enable_preview = true,
+				},
+			},
+		})
 
-    local builtin = require("telescope.builtin")
-    local keymap = vim.keymap
-    local uv = vim.uv or vim.loop
+		local builtin = require("telescope.builtin")
+		local keymap = vim.keymap
+		local uv = vim.uv or vim.loop
 
-    local function safe_cwd()
-      local cwd = uv and uv.cwd and uv.cwd() or nil
-      if type(cwd) ~= "string" or cwd == "" then
-        cwd = vim.fn.getcwd()
-      end
-      if type(cwd) ~= "string" or cwd == "" then
-        cwd = vim.fn.expand("~")
-      end
-      return cwd
-    end
+		local function safe_cwd()
+			local cwd = uv and uv.cwd and uv.cwd() or nil
+			if type(cwd) ~= "string" or cwd == "" then
+				cwd = vim.fn.getcwd()
+			end
+			if type(cwd) ~= "string" or cwd == "" then
+				cwd = vim.fn.expand("~")
+			end
+			return cwd
+		end
 
-    local function with_cwd(fn)
-      return function()
-        fn({ cwd = safe_cwd() })
-      end
-    end
+		local function with_cwd(fn)
+			return function()
+				fn({ cwd = safe_cwd() })
+			end
+		end
 
-    keymap.set("n", "<leader>ff", with_cwd(builtin.find_files), { desc = "Find Files" })
-    keymap.set("n", "<leader>fg", with_cwd(builtin.live_grep), { desc = "Live Grep" })
-    keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-    keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
-    keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent Files" })
-    keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
-    keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
-    keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, { desc = "Search Current Buffer" })
-    keymap.set("n", "<leader>fw", with_cwd(builtin.grep_string), { desc = "Word Under Cursor" })
-    keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
-  end,
+		keymap.set("n", "<leader>ff", with_cwd(builtin.find_files), { desc = "Find Files" })
+		keymap.set("n", "<leader>fg", with_cwd(builtin.live_grep), { desc = "Live Grep" })
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent Files" })
+		keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+		keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
+		keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, { desc = "Search Current Buffer" })
+		keymap.set("n", "<leader>fw", with_cwd(builtin.grep_string), { desc = "Word Under Cursor" })
+		keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
+		keymap.set("n", "<leader>ut", builtin.colorscheme, { desc = "Colorschemes" })
+	end,
 }
