91a125b2e4492b168196c92f776e4587a42c59a1
Sn <164057221+nzrrs@users.noreply.github.com>
2026-07-23 15:50:05 +0100

    Update colortheme.lua

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -1,24 +1,26 @@
-local CURRENT_THEME = "tokyonight" -- Change this
+local theme = require("theme")
+
+local function apply_theme(name)
+	vim.cmd.colorscheme(name)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
 return {
-	{ "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+	{ "folke/tokyonight.nvim" },
 	{ "catppuccin/nvim", name = "catppuccin" },
 	{ "rebelot/kanagawa.nvim" },
 	{ "shaunsingh/nord.nvim" },
 	{ "ellisonleao/gruvbox.nvim" },
 	{ "EdenEast/nightfox.nvim" },
 	{ "Mofiqul/vscode.nvim" },
-	{ "projekt0n/github-nvim-theme" },
 
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme(CURRENT_THEME)
-
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			apply_theme(theme.current)
 		end,
 	},
 }
